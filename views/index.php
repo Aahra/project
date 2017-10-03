d01f22b5422893dc5b9040ca28865d190f4587da
jonsnow <jonsnow@localhost.localdomain>
2017-10-03 01:22:36 -0400

    navigation added

--- views/index.php
+++ views/index.php
@@ -10,14 +10,16 @@
 <head>
     <title></title>
     <link rel="stylesheet" href="css/index.css">
+    <script src="./script/index.js"></script>
 </head>
+
 <body>
 <div id="header">
     <div class="logo">
         <ul>
             <li>
                 <img src="../images/hm.png">
-                <a href="#">Dash<span>Logo</span></a>
+                <a href="#">Clever<span>Investor</span></a>
 
 
             </li>
@@ -28,6 +30,45 @@
     <div class="man">
         <img src="../images/man.png">
     </div>
+
+</div>
+<div class="navbar">
+<ul>
+    <li>
+        <a href="#">Home</a>
+    </li>
+    <li>
+        <a href="#">Contact</a>
+    </li>
+    <li>
+        <a href="signup.php">Signup</a>
+    </li>
+    <li>
+        <a href="login.php">Login</a>
+    </li>
+    <li>
+        <a href="#">Search</a>
+    </li>
+
+
+
+</ul>
+
+</div>
+
+<div id="slideshow">
+
+
+        <img class="slides" src="../images/ek.jpeg">
+        <img class="slides" src="../images/do.jpeg">
+        <img class="slides" src="../images/teen.jpeg">
+        <img class="slides" src="../images/char.jpeg">
+        <img class="slides" src="../images/panch.jpeg">
+
+
+
+
+
 </div>
 
 </body>
---
+++ views/script/index.js
@@ -0,0 +1,21 @@
+//alert("hell yeah");
+var index=0;
+slide();
+function slide() {
+    var i;
+    var x=document.getElementById("slideshow");
+    alert(x);
+    for(i=0;i<x.length;i++)
+    {
+        x[i].style.display="none";
+    }
+    index++;
+    if(index>x.length)
+    {
+        index=1;
+    }
+    x[index-1].style.display="block";
+    setTimeout(slide,2000);
+
+
+}

c4b3391c32efcf0ecb671656ea38bf23fe149691
jonsnow <jonsnow@localhost.localdomain>
2017-10-02 20:54:20 -0400

    minor changes

---
+++ controllers/indexController.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jonsnow
+ * Date: 10/2/17
+ * Time: 4:55 PM
+ * --->>>this controller is for handling index request
+ */
---
+++ views/index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jonsnow
+ * Date: 10/2/17
+ * Time: 5:03 PM
+ */
+?>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+<div id="header">
+    <div class="logo">
+        <ul>
+            <li>
+                <img src="../images/hm.png">
+                <a href="#">Dash<span>Logo</span></a>
+
+
+            </li>
+        </ul>
+
+
+    </div>
+    <div class="man">
+        <img src="../images/man.png">
+    </div>
+</div>
+
+</body>
+</html>
